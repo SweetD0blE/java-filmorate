4cc1a209fe5cd7d8e53383688c3887234910af1c
cam1234251 <cam1234251@mail.ru>
2023-04-07 13:57:08 +0300

    edit ErrorHandler1

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -29,7 +29,7 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleServiceException(final ServiceException e) {
         return new ErrorResponse(
                 e.getMessage()
