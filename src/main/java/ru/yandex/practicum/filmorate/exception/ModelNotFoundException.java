97aae5b99ec1875705825ca38baaf0f3b0e8d5e3
cam1234251 <cam1234251@mail.ru>
2023-05-10 19:04:08 +0300

    PR problems #10

--- src/main/java/ru/yandex/practicum/filmorate/exception/ModelNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class ModelNotFoundException extends RuntimeException {
-
-    public ModelNotFoundException(String message) {
-        super(message);
-    }
-}
